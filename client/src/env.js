02328cdc5628baa2aeb5c50334ff4774b46e52a9
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2024-01-24 15:25:36 -0700

    Fixed Prod server

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7045' : 'giazilla.com'
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'strangkolby.us.auth0.com'
 export const clientId = 'eRyKTI5HCdDh7BqM0hj63QD4UMQ2wU7X'

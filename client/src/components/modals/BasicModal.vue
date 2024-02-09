a4f47e870125a1a175b4773e099e442dba8cf957
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2024-02-09 16:02:34 -0700

    Removed Description for now and added some admin things

---
+++ client/src/components/modals/BasicModal.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="modal fade" :id="_id" tabindex="-1" :aria-labelledby="_id + 'Label'" aria-hidden="true">
+        <div class="modal-dialog" :class="size">
+            <div class="modal-content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        _id: { required: true },
+        size: { type: String }
+    }
+};
+</script>

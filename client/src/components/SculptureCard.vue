dffa89febe08752fc941e8116e8c80e252a95fb0
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2024-02-09 14:46:45 -0700

    Finished Quick loader

--- client/src/components/SculptureCard.vue
+++ client/src/components/SculptureCard.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container-fluid border-secondary px-0 position-relative">
         <router-link :to="`sculpture/${sculpture.id}`" class="h-100 hover">
-            <FastImage :_id="sculpture.id" :_class="'aspect-1-1 img-fluid fit-cover rounded'" :_src="sculpture.coverImg"
-                :_alt="sculpture.name"></FastImage>
+            <FastImage :_id="sculpture.id" :_class="'aspect-1-1 img-fluid fit-cover w-100 h-100 rounded'"
+                :_src="sculpture.coverImg" :_alt="sculpture.name"></FastImage>
             <div class="fw-bold text-shadow text-white cover">
                 <p class="reveal cover-title mb-0 fs-4 w-75">{{ sculpture.name }}</p>
                 <p class="reveal cover-year mb-0">{{ sculpture.year }}</p>

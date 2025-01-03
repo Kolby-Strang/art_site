02933f420d1efe02dad17da662e69edcd147abe0
Kolby-Strang <strangkolby@gmail.com>
2025-01-02 20:35:49 -0700

    again cause im turbo dumb

--- client/src/pages/SculpturePage.vue
+++ client/src/pages/SculpturePage.vue
@@ -48,11 +48,11 @@
             <div class="col-12 col-md-2 order-1 order-md-0 fs-6">
                 <p>Made in: {{ sculpture.year }}</p>
                 <p v-if="sculpture.dimensions">{{ sculpture.dimensions }}</p>
-                <div v-if="sculpture.price">
+                <p v-if="sculpture.price == 69">These pieces are available at Idaho Art Gallery with this link to their website, www.idahoartgallery.com</p>
+                <div v-else-if="sculpture.price">
                     <button class="btn btn-secondary">${{ sculpture.price }}</button>
                     <p class="fs-7">*Contact me if you're interested in purchasing!</p>
                 </div>
-                <p v-else-if="sculpture.price == 69">These pieces are available at Idaho Art Gallery with this link to their website, www.idahoartgallery.com</p>
                 <p v-else>Sold</p>
             </div>
             <div v-if="false" class="col-12 col-md-10 order-0 order-md-1 border-secondary">
